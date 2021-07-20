f3b788cd0e8cdfadd0fd11756c3f8096bee59c3f
Arark <knkr5456@gmail.com>
2021-07-20 14:35:22 +0900

    ch3

---
+++ common/util.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+def preprocess(text: str):
+    text = text.lower()
+    text = text.replace(".", " .")
+    words = text.split(" ")
+    word2id: dict[str, int] = {}
+    id2word: dict[int, str] = {}
+    for word in words:
+        if word not in word2id:
+            new_id = len(word2id)
+            word2id[word] = new_id
+            id2word[new_id] = word
+
+    corpus = np.array([word2id[w] for w in words])
+    return corpus, word2id, id2word
+
+
+def convert_one_hot(corpus, vocab_size):
+    '''one-hot表現への変換
+    :param corpus: 単語IDのリスト（1次元もしくは2次元のNumPy配列）
+    :param vocab_size: 語彙数
+    :return: one-hot表現（2次元もしくは3次元のNumPy配列）
+    '''
+    N = corpus.shape[0]
+
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
+
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, word_ids in enumerate(corpus):
+            for idx_1, word_id in enumerate(word_ids):
+                one_hot[idx_0, idx_1, word_id] = 1
+
+    return one_hot
+
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
+
+def create_contexts_target(corpus, window_size=1):
+    target=corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(window_size, len(corpus)-window_size):
+        cs = []
+        for t in range(-window_size, window_size+1):
+            if t==0:
+                continue
+            cs.append(corpus[idx+t])
+        contexts.append(cs)
+    return np.array(contexts), np.array(target)
