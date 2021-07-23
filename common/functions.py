6265ff059e6e206b05e9f6cd44521107dd27c674
Arark <knkr5456@gmail.com>
2021-07-24 02:29:13 +0900

    typing functions.py

--- common/functions.py
+++ common/functions.py
@@ -2,15 +2,15 @@
 import numpy as np
 
 
-def sigmoid(x):
+def sigmoid(x: np.ndarray):
     return 1 / (1 + np.exp(-x))
 
 
-def relu(x):
+def relu(x: np.ndarray):
     return np.maximum(0, x)
 
 
-def softmax(x):
+def softmax(x: np.ndarray):
     if x.ndim == 2:
         x = x - x.max(axis=1, keepdims=True)
         x = np.exp(x)
