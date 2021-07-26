ed74b6b0310899547cb699f484a610ebd4b489c0
Arark <knkr5456@gmail.com>
2021-07-26 10:46:24 +0900

    feat: impl LSTM, TimeLSTM

--- common/time_layers.py
+++ common/time_layers.py
@@ -1,4 +1,4 @@
-from common.functions import softmax
+from common.functions import sigmoid, softmax
 from typing import Optional
 import numpy as np
 from .layers import Embedding
@@ -203,3 +203,126 @@ class TimeSoftmaxWithLoss:
         dx = dx.reshape((N, T, V))
 
         return dx
+
+
+class LSTM:
+    def __init__(self, Wx: np.ndarray, Wh: np.ndarray, b: np.ndarray):
+        self.params = (Wx, Wh, b)
+        self.grads = (np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b))
+        self.cache = None
+
+    def forward(self, x: np.ndarray, h_prev: np.ndarray, c_prev: np.ndarray):
+        Wx, Wh, b = self.params
+        N, H = h_prev.shape
+
+        A = x @ Wx + h_prev @ Wh + b
+
+        f = A[:, :H]
+        g = A[:, H:2*H]
+        i = A[:, 2*H: 3*H]
+        o = A[:, 3*H:]
+
+        f = sigmoid(f)
+        g = np.tanh(g)
+        i = sigmoid(i)
+        o = sigmoid(o)
+
+        c_next = f * c_prev + g*i
+        h_next = o*np.tanh(c_next)
+
+        self.cache = (x, h_prev, c_prev, i, f, g, o, c_next)
+        return h_next, c_next
+
+    def backward(self, dh_next: np.ndarray, dc_next: np.ndarray):
+        Wx, Wh, b = self.params
+        x, h_prev, c_prev, i, f, g, o, c_next = self.cache
+
+        tanh_c_next = np.tanh(c_next)
+
+        ds = dc_next + (dh_next * o) * (1 - tanh_c_next ** 2)
+
+        dc_prev = ds * f
+
+        di = ds * g
+        df = ds * c_prev
+        do = dh_next * tanh_c_next
+        dg = ds * i
+
+        di *= i * (1 - i)
+        df *= f * (1 - f)
+        do *= o * (1 - o)
+        dg *= (1 - g ** 2)
+
+        dA = np.hstack((df, dg, di, do))
+
+        dWh = h_prev.T @ dA
+        dWx = x.T @ dA
+        db = dA.sum(axis=0)
+
+        self.grads[0][...] = dWx
+        self.grads[1][...] = dWh
+        self.grads[2][...] = db
+
+        dx = dA @ Wx.T
+        dh_prev = dA @ Wh.T
+
+        return dx, dh_prev, dc_prev
+
+
+class TimeLSTM:
+    def __init__(self, Wx: np.ndarray, Wh: np.ndarray, b: np.ndarray, stateful=False) -> None:
+        self.params = (Wx, Wh, b)
+        self.grads = (np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b))
+        self.layers: list[LSTM] = []
+        self.h: np.ndarray = None
+        self.c: np.ndarray = None
+        self.dh: np.ndarray = None
+        self.stateful = stateful
+
+    def forward(self, xs: np.ndarray):
+        Wx, Wh, b = self.params
+        N, T, D = xs.shape
+        H = Wh.shape[0]
+
+        self.layers = []
+        hs = np.empty((N, T, H), dtype=np.float_)
+
+        if not self.stateful or self.h is None:
+            self.h = np.zeros((N, H), dtype=np.float_)
+        if not self.stateful or self.c is None:
+            self.c = np.empty((N, H), dtype=np.float_)
+
+        for t in range(T):
+            layer = LSTM(*self.params)
+            self.h, self.c = layer.forward(xs[:, t, :], self.h, self.c)
+            hs[:, t, :] = self.h
+
+            self.layers.append(layer)
+
+        return hs
+
+    def backward(self, dhs: np.ndarray):
+        Wx, Wh, b = self.params
+        N, T, H = dhs.shape
+        D = Wx.shape[0]
+
+        dxs = np.empty((N, T, D), dtype=np.float_)
+        dh, dc = 0, 0
+        grads = [0, 0, 0]
+        for t in reversed(range(T)):
+            layer = self.layers[t]
+            dx, dh, dc = layer.backward(dhs[:, t, :] + dh, dc)
+            dxs[:, t, :] = dx
+            for i, grad in enumerate(layer.grads):
+                grads[i] += grad
+
+        for i, grad in enumerate(grads):
+            self.grads[i][...] = grad
+        self.dh = dh
+        return dxs
+
+    def set_state(self, h: np.ndarray, c: np.ndarray = None):
+        self.h, self, c = h, c
+
+    def reset_state(self):
+        self.h, self.c = None, None
